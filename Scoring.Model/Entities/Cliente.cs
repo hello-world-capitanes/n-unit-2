2e1d9c303c56c43b0898783a817bff2bde957fdb
BABEL-SI\andres.salazar <andres.salazar@babelgroup.com>
2023-10-18 11:07:01 +0200

    init Models

---
+++ Scoring.Model/Entities/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Scoring.Model.Entities
+{
+    public class Cliente
+    {
+
+        public DateTime FechaDeNacimiento { get; set; }
+        public double IngresosNetosAnualesAsalariado { get; set; }
+        public double IngresosNetosAnualesAutonomo { get; set; }
+        public double IngresosBrutosAnualesAutonomo { get; set; }
+        public string Nacionalidad { get; set; }
+        public string CifEmpleador { get; set; }
+        
+
+    }
+}
---
+++ Scoring.Model/Entities/Solicitud.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scoring.Model.Entities
+{
+    public class Solicitud
+    {
+        public DateTime Fecha { get; set; }
+        public double Inversion { get; set; }
+        public double Cuota {  get; set; }
+        public int Plazo { get; set; }
+        public DateTime InicioRenting { get; set; }
+        
+        public Cliente cliente { get; set; }
+    }
+}
